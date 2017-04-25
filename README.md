a74b9cbb4cada55707c4913e757740f009dd2fb0
Harkann <paulon@crans.org>
2017-04-25 18:19:13 +0200

    Poste sur twitter+fb

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SocialMediaManager
 Outil de gestion des réseaux sociaux (Facebook, Twitter, G+, WikiCrans ...)
+
+Dépendancies :
+ * facebook-sdk
+ * python-twitter
+
+(installed using pip3)
--- socialmediamanager.py
+++ socialmediamanager.py
@@ -1,33 +1,31 @@
 #!/usr/bin/env python3
 
-#Permet de poster sur facebook.
-#Required : facebook-sdk
+import facebook #require facebook-sdk
+import twitter #require python-twitter
 
-
-import facebook
 import argparse
-import credentials
+import credentials as cr
 
 def post_facebook(message_to_post):
 	''' Poste le message sur facebook'''
-	token = credentials.facebook_token
-	graph = facebook.GraphAPI(access_token=token, version='2.9')
-	graph.put_object(parent_object='me', connection_name='feed', message=message_to_post)
+	api_fb = facebook.GraphAPI(access_token=cr.facebook_token, version='2.9')
+	api_fb .put_object(parent_object='me', connection_name='feed', message=message_to_post)
 
+def post_twitter(message_to_post):
+	'''Poste le message sur twitter'''
+	api_tw = twitter.Api(consumer_key=cr.twitter_consumer_key,
+                  consumer_secret=cr.twitter_consumer_secret,
+                  access_token_key=cr.twitter_access_token,
+                  access_token_secret=cr.twitter_access_token_secret)
+	api_tw.PostUpdate(message_to_post)
 
-
-
-
-
-
-
-
-
-
+def post_google(message_to_post):
+	'''Poste le message sur google+'''
+	print('WIP')
 
 
 #parseur
-parser = argparse.ArgumentParser(description='Plop')
+parser = argparse.ArgumentParser(description='Script permettant de poster un messge sur les réseaux sociaux spécifiés')
 parser.add_argument('-f', '--facebook', help='poste sur facebook', action='store_true')
 parser.add_argument('-t', '--twitter', help='poste sur twitter', action='store_true')
 parser.add_argument('-g', '--google', help='poste sur google+', action='store_true')
@@ -42,3 +40,5 @@ args = parser.parse_args()
 if args.message_to_post :
 	if args.facebook :
 		post_facebook(args.message_to_post)
+	if args.twitter :
+		post_twitter(args.message_to_post)
